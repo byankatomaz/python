ea1ea0fb9ec9aa7ecf94f0a709ef36db94a9c986
Byanka Tomaz <125160119+byankatm@users.noreply.github.com>
2023-05-05 11:29:02 -0300

    Update ChatBotOpenai.py

--- Jogos/CaraACara/ChatBotOpenai.py
+++ Jogos/CaraACara/ChatBotOpenai.py
@@ -1,106 +1,62 @@
 import openai
-from senha import API_KEY
 import random
+from senha import API_KEY
 
-# Configurar a API Key da OpenAI
+# Defina a sua chave de API do OpenAI
 openai.api_key = f"{API_KEY}"
 
-# Lista de personagens com suas características
-personagens = [
-    {
-        "nome": "Mario",
-        "altura": "1,55m",
-        "idade": "26 anos",
-        "personalidade": "amigável, corajoso e altruísta"
-    },
-    {
-        "nome": "Luigi",
-        "altura": "1,76m",
-        "idade": "27 anos",
-        "personalidade": "tímido, gentil e inteligente"
-    },
-    {
-        "nome": "Princesa Peach",
-        "altura": "1,73m",
-        "idade": "24 anos",
-        "personalidade": "bondosa, inteligente e leal"
-    },
-    # adicione mais personagens aqui
-]
+# Defina o modelo que você deseja usar
+model_engine = "ada"
 
-# Função para sortear um personagem aleatoriamente
-def sortear_personagem():
-    return random.choice(personagens)
+# Defina a lista de personagens disponíveis
+personagens = ["Goku", "Harry Potter", "Homem-Aranha", "Darth Vader", "Superman", "Batman"]
 
-def gerar_resposta(personagem, pergunta):
-    # Consultar a API da OpenAI para gerar uma resposta com base nas características do personagem
-    prompt = f"Vamos brincar de Cara A Cara(Quem sou eu?), lembre-se das regras. Esse é o meu personagem: {personagem['nome']}. Vou fazer perguntas sobre ele: {pergunta}, e você responde com sim ou não de acordo com as caracteristicas dele. Nunca fale o nome do personagem"
+# Escolha um personagem aleatório para o jogo
+personagem_secreto = random.choice(personagens)
+
+
+# Defina a função para enviar a solicitação API e obter a resposta
+def ask_gpt(prompt):
     response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine=model_engine,
         prompt=prompt,
-        temperature=0.5,
-        max_tokens=50,
+        max_tokens=1024,
         n=1,
         stop=None,
-        timeout=10,
+        temperature=0.7,
     )
-    resposta = response.choices[0].text.strip()
-
-    # Verificar se a resposta contém apenas "yes" ou "no", e retornar "Sim" ou "Não"
-    if resposta.lower() == "yes":
-        return "Sim"
-    elif resposta.lower() == "no":
-        return "Não"
-    else:
-        return resposta
+    return response.choices[0].text.strip()
 
 
-# Fluxo de jogo
+# Inicie o jogo de adivinhação
+print("Bem-vindo ao jogo 'Quem sou eu?'")
+print("Você tem que adivinhar qual personagem estou pensando. Faça perguntas que possam ser respondidas com 'sim' ou 'não'.")
+print("Para desistir, digite 'desisto'.")
+
+sum = 0
+
 while True:
-    # Sortear um personagem aleatoriamente
-    personagem_sorteado = sortear_personagem()
+    pergunta = input("Sua pergunta: ")
+    sum += 1
 
-    # Iniciar o jogo
-    print("Jogo Cara a Cara: Quem sou eu?")
-    print(f"Eu sou um personagem {personagem_sorteado['nome']}.")
+    if pergunta.lower() == "desisto":
+        print(f"O personagem era {personagem_secreto}. Tente novamente da próxima vez!")
+        break
 
-    # Controlar o número de perguntas feitas e o número de tentativas
-    num_perguntas = 0
-    num_tentativas = 0
-    dicas = []
+    resposta = ask_gpt(f"Vamos jogar um jogo chamado Cara a Cara(Quem sou eu?)\n Eu sou o {personagem_secreto}, mas não sei disso.\n Essa é minha pergunta sobre ele para eu tentar acertar quem sou eu: {pergunta}?\n Responda apenas com sim ou não baseado nas caracteristicas do meu personagem.")
 
-    # O usuário faz perguntas para tentar adivinhar quem é o personagem
-    acertou = False
-    while not acertou:
-        pergunta = input("Qual sua pergunta? ")
-        resposta = gerar_resposta(personagem_sorteado, pergunta)
+    if sum == 3:
+        resposta = ask_gpt(f"Chat, me de UMA dica sobre a caracteristica do personagem, apenas UMA, por exemplo: 'Você tem um desenho sobre você'.")
         print(resposta)
 
-        # Adicionar a pergunta às dicas após 3 perguntas
-        num_perguntas += 1
-        if num_perguntas == 3:
-            dicas.append(pergunta)
-
-        # Verificar se o usuário adivinhou corretamente
-        if personagem_sorteado["nome"] in pergunta:
-            acertou = True
-        else:
-            num_tentativas += 1
-
-        # Informar ao usuário que ele perdeu após 5 tentativas
-        if num_tentativas == 5:
-            print(f"Você perdeu! O personagem era {personagem_sorteado['nome']}.")
-            break
-
-    # Se o usuário acertou, informar o número de tentativas e as dicas
-    if acertou:
-        print(f"Parabéns, você acertou em {num_tentativas} tentativas!")
-        if dicas:
-            print("Dicas:")
-            for dica in dicas:
-                print(f"- {dica}")
-
-    # Perguntar se o usuário quer jogar novamente
-    jogar_novamente = input("Deseja jogar novamente? (s/n) ")
-    if jogar_novamente.lower() != "s":
+    if personagem_secreto in pergunta.title():
+        print("Parabens você acertou")
         break
+
+    if "sim" in resposta.lower():
+        print("Sim")
+    elif "não" in resposta.lower():
+        print("Não")
+    else:
+        print(f"Modelo respondeu: {resposta}. Tente fazer outra pergunta.")
+
