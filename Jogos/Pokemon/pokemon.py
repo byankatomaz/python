b7508d809c735a6e1a68ef745d8a2c84c774f225
Byanka Tomaz <125160119+byankatm@users.noreply.github.com>
2023-05-18 17:02:39 -0300

    Update pokemon.py

--- Jogos/Pokemon/pokemon.py
+++ Jogos/Pokemon/pokemon.py
@@ -1,35 +1,28 @@
 class Pokemon():
-    def __init__(self, name, attack, defense, speed):
+    def __init__(self, name, life, attack, defense, speAttack, speDefense, speed):
         self.type = ''
         self.name = name
         self.attack = attack
         self.defense = defense
         self.speed = speed
+        self.life = life
+        self.speAttack = speAttack
+        self.speDefense = speDefense
 
 
 class Water(Pokemon):
     type = 'Water'
     def __init__(self, name, life, attack, defense, speAttack, speDefense, speed):
-        super().__init__(name, attack, defense, speed)
-        self.life = life
-        self.speAttack = speAttack
-        self.speDefense = speDefense
+        super().__init__(name, life, attack, defense, speAttack, speDefense, speed)
 
 
 class Grass(Pokemon):
     type = 'Grass'
     def __init__(self, name, life, attack, defense, speAttack, speDefense, speed):
-        super().__init__(name, attack, defense, speed)
-        self.life = life
-        self.speAttack = speAttack
-        self.speDefense = speDefense
+        super().__init__(name, life, attack, defense, speAttack, speDefense, speed)
     
 
 class Fire(Pokemon):
     type = 'Fire'
     def __init__(self, name, life, attack, defense, speAttack, speDefense, speed):
-        super().__init__(name, attack, defense, speed)
-        self.life = life
-        self.speAttack = speAttack
-        self.speDefense = speDefense
-    
+        super().__init__(name, life, attack, defense, speAttack, speDefense, speed)
