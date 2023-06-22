3bae9e6ebdeaecf2564e73951cc906d7933881e6
Byanka Tomaz <125160119+byankatm@users.noreply.github.com>
2023-06-22 14:17:09 -0300

    Update dino.py

--- Jogos/Teste2/dino.py
+++ Jogos/Teste2/dino.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.locals import *
 from sys import exit
-from random import randrange
+from random import randrange, choice
 import os
 
 WIDTH = 640
@@ -18,6 +18,10 @@ class Supreme:
         self.sprite_sheet = pygame.image.load(os.path.join(self.images_directory, 'dinoSpritesheet.png')).convert_alpha()
 
         self.som_pulo = pygame.mixer.Sound(os.path.join(self.songs_directory, 'jump_sound.wav'))
+        
+        self.som_colisao = pygame.mixer.Sound(os.path.join(self.songs_directory, 'death_sound.wav'))
+        
+        self.escolha_obstaculo = choice([0, 1])
 
 class Chao(pygame.sprite.Sprite):
     def __init__(self, pos_x):
@@ -36,6 +40,64 @@ class Chao(pygame.sprite.Sprite):
         if self.rect.topright[0] < 0:
             self.rect.x = WIDTH
         self.rect.x -= 10
+
+class Cacto(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.sprite_sheet = Supreme().sprite_sheet
+        
+        self.image = self.sprite_sheet.subsurface((5 * 32, 0), (32, 32))
+        self.image = pygame.transform.scale(self.image, (32 * 2, 32 * 2))
+        
+        self.rect = self.image.get_rect()
+        
+        self.mask =  pygame.mask.from_surface(self.image)
+        self.escolha = Supreme().escolha_obstaculo
+        
+        self.rect.center = (WIDTH, HEIGHT - 92)
+        self.rect.x = WIDTH
+    
+    def update(self): 
+        if self.escolha == 0:
+            if self.rect.topright[0] < 0:
+                self.rect.x = WIDTH
+            self.rect.x -= 10
+
+class DinoVoador(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.sprite_sheet = Supreme().sprite_sheet
+        
+        self.images_dinoVoador = []
+        
+        for i in range(3, 5):
+            img = self.sprite_sheet.subsurface((i * 32, 0), (32, 32))
+            img = pygame.transform.scale(img, (32*3, 32*3))
+            self.images_dinoVoador.append(img)
+        
+        self.index_lista = 0
+        self.image = self.images_dinoVoador[self.index_lista]
+        
+        self.mask =  pygame.mask.from_surface(self.image)
+        self.escolha = Supreme().escolha_obstaculo
+        
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH, 300)
+        self.rect.x = WIDTH
+    
+    def update(self):
+        if self.escolha == 1:
+            if self.rect.topright[0] < 0:
+                self.rect.x = WIDTH
+            self.rect.x -= 10
+            
+            if self.index_lista > 1:
+                self.index_lista = 0
+            self.index_lista += 0.25
+
+            self.image = self.images_dinoVoador[int(self.index_lista)]
 
 
 class Dinossauro(pygame.sprite.Sprite):
@@ -58,6 +120,9 @@ class Dinossauro(pygame.sprite.Sprite):
 
         self.pos_y_inicial = 390 - 96 // 2
         self.rect = self.image.get_rect()
+        
+        self.mask =  pygame.mask.from_surface(self.image)
+        
         self.rect.center = (100, self.pos_y_inicial)
 
         self.pulo = False
@@ -113,6 +178,7 @@ class Dino:
     def __init__(self):
 
         self.todas_as_sprites = pygame.sprite.Group()
+        self.grupo_obstaculos = pygame.sprite.Group()
 
         self.RED = 255, 0, 0
         self.GREEN = 0, 255, 0
@@ -123,6 +189,7 @@ class Dino:
         self.height = HEIGHT
 
         self.pulo = False
+        self.colidiu = False
 
         self.clock = pygame.time.Clock()
 
@@ -144,6 +211,14 @@ class Dino:
         for i in range(4):
             self.nuvem = Nuvens()
             self.todas_as_sprites.add(self.nuvem)
+            
+        self.cacto = Cacto()
+        self.todas_as_sprites.add(self.cacto)
+        self.grupo_obstaculos.add(self.cacto)
+        
+        self.dinoVoador = DinoVoador()
+        self.todas_as_sprites.add(self.dinoVoador)
+        self.grupo_obstaculos.add(self.dinoVoador)
 
         self.running()
 
@@ -155,7 +230,20 @@ class Dino:
 
             self.windowP.fill(self.WHITE)
             self.interaction()
+            
+            self.colisoes =  pygame.sprite.spritecollide(self.dinossauro, self.grupo_obstaculos, False, pygame.sprite.collide_mask)
+            
             self.drawing()
+            
+            if self.cacto.rect.topright[0] <= 0 or self.dinoVoador.rect.topright[0] <= 0:
+                self.escolha_obstaculo = choice([0, 1])
+                self.cacto.x = WIDTH
+                self.dinoVoador.x = WIDTH
+                
+                self.cacto.escolha =  self.escolha_obstaculo
+                self.dinoVoador.escolha =  self.escolha_obstaculo
+            
+            self.collide()
 
             pygame.display.flip()
 
@@ -172,13 +260,23 @@ class Dino:
                         pass
                     else:
                         self.dinossauro.pular()
+    
+    def collide(self):
 
-    #
-    # def pulando(self):
+        if self.colisoes and self.colidiu == False:
+            Supreme().som_colisao.play()
+            self.colidiu = True
+        
+        elif self.colidiu == True:
+            pass
+        
+        else:
+            self.update()
 
     def drawing(self):
-
         self.todas_as_sprites.draw(self.windowP)
+    
+    def update(self):
         self.todas_as_sprites.update()
 
 
